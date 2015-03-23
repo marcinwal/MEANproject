e1abbdf491ecd84426eba77fe950e5f63f022ad8
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-23 20:01:14 +0000

    working refactored

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var http = require('http');
+var path = require('path');
+var favicon = require('favicon');
+var logger = require('logger');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// var expressLayouts = require('express-ejs-layouts');
+
+
+
+app.set('port',process.env.PORT || 3000);
+app.set('views',path.join(__dirname,'/app_server/views'));
+app.set('view engine','jade');
+// app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+// app.use(expressLayouts);  
+
+app.use(express.static(__dirname,'/public')).use(cookieParser());
+
+
+
+require('./routes')(app);
+
+server.listen(app.get('port'),function(){
+  console.log("Server is runnin at" + app.get('port'));
+});
+
+module.exports = server;
