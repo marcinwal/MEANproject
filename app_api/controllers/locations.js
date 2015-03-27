1e6ba5cefd6f61d0ba75b3076aae4dbe6c4e2bbd
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-27 17:29:35 +0000

    api read locationid added

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -16,8 +16,22 @@ module.exports.locationsListByDistance = function(req,res){
 
 };
 module.exports.locationsReadOne = function(req,res){
-  sendJsonResponse(res,200,{"status": "success"});
+  if(req.params && req.params.locationid){
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err,location){
+        if(!location){
+          sendJsonResponse(res,404,{"message": "locationid not found"});
+        }else if(err){
+          sendJsonResponse(res,)
+        }else
+        sendJsonResponse(res,200,location);
+      });
+  } else {
+    sendJsonResponse(res,404,{"message": "No locationid provided"});
+  }  
 };
+
 module.exports.locationsUpdateOne = function(req,res){
 
 };
@@ -34,5 +48,5 @@ module.exports.reviewsUpdateOne = function(req,res){
 
 };
 module.exports.reviewsDeleteOne = function(req,res){
-  
+
 };
