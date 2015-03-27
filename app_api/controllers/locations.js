2c0f6b2be26e7b2f34b4520e569df972c7ddada2
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-27 17:48:33 +0000

    adding reading review for given location and review

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -42,6 +42,43 @@ module.exports.reviewsCreate = function(req,res){
 
 };
 module.exports.reviewsReadOne = function(req,res){
+  if(req.params && req.params.locationid && req.params.reviewid)
+  {
+    Loc
+      .findById(req.params.locationid)
+      .select('name review')
+      .exec(function(err,location){
+        var response, review;
+        if(!location){
+          sendJsonResponse(res,404,{"message": "locationid not found"});
+          return;
+        } else if(err){
+          sendJsonResponse(res,400,err);
+          return;
+        }
+        if(location.reviews && location.reviews.length > 0){
+          review = location.reviews.id(req.params.reviewid);
+          if (!review){
+            sendJsonResponse(res,404,{"message": "not found"});
+          } else {
+            response = {
+              location : {
+                name : location.name,
+                id : req.params.locationid
+              },
+              review : review
+            };
+            sendJsonResponse(res,200,response);
+          }
+        } else {
+          sendJsonResponse(res,404,{"message": "No reviews found"});
+        }
+      });
+  }else
+  {
+    sendJsonResponse(res,404,{"message": "wrong parameters"});
+  }
+
 
 };
 module.exports.reviewsUpdateOne = function(req,res){
