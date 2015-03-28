569f6354e768e2aa2ad329aa93b000b272668191
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-28 22:07:36 +0000

    creating a location

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -24,7 +24,30 @@ var sendJsonResponse = function(res,status,content){
 }
 
 module.exports.locationsCreate = function(req,res){
-  sendJsonResponse(res,200,{"status": "success"});
+  Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.lng),parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      openinig: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1
+    },{
+      days: req.body.days2,
+      openinig: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2
+    }]
+  },function(err,location){
+    if (err){
+      sendJsonResponse(res,400,err);
+    }else
+    {
+      sendJsonResponse(res,201,location);
+    }    
+  });
 };
 
 
