2afe7b7923e7d834e209f8c0e9d5e10ef3ffe563
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-28 22:28:18 +0000

    adding rating reviews save

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -18,6 +18,62 @@ var theEarth = (function(){
 
 })();
 
+
+var doSetAverageRating = function(location){
+  var i,reviewCount, ratingAverage, ratingTotal;
+  if(location.reviews && location.reviews.length > 0){
+    reviewCount = location.reviews.length;
+    ratingTotal = 0;
+    for(i = 0; i < review.count; i++){
+      ratingTotal += location.reviews[i].rating;
+    }
+    ratingAverage = parseInt(ratingTotal / reviewCount,10);
+    location.rating = ratingAverage;
+    location.save(function(err){
+      if(err){
+        console.log(err);
+      } else 
+      {
+        console.log("Average rating is " + ratingAverage);
+      }
+    });
+  }
+};
+
+var updateAverageRating = function(locationid){
+  Loc
+    .findById(locationid)
+    .select('rating reviews')
+    .exec(function(err,location){
+      if(err){
+        doSetAverageRating(location);
+      }
+    });
+};
+
+var doAddReview = function(req,res,location){
+  if(!location){
+    sendJsonResponse(res,404,{"message": "location not found"});
+  } else {
+    location.reviews.push({
+      author: {
+        displayName: req.body.author
+      },
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    });
+    location.save(function(err,location){
+      if(err){
+        sendJsonResponse(res,404,err);
+      }else {
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length -1];
+        sendJsonResponse(res,201,thisReview);
+      }
+    });
+  }
+};
+
 var sendJsonResponse = function(res,status,content){
   res.status(200);
   res.json(content);
@@ -113,9 +169,30 @@ module.exports.locationsUpdateOne = function(req,res){
 module.exports.locationsDeleteOne = function(req,res){
 
 };
-module.exports.reviewsCreate = function(req,res){
 
+
+
+
+module.exports.reviewsCreate = function(req,res){
+  var locationid = req.params.locationid;
+  if(locationid) {
+    Loc
+      .findById(locationid)
+      .select('reviews')
+      .exec(
+        function(err,location) {
+          if(err) {
+            sendJsonResponse(res,400,err);
+          } else {
+            doAddReview(req,res,location);
+          }
+        }
+    );
+  } else {
+    sendJsonResponse(res,404,{"message": "not found "});
+  }
 };
+
 module.exports.reviewsReadOne = function(req,res){
   if(req.params && req.params.locationid && req.params.reviewid)
   {
