83c9826f502899a980b38688408c9466b2746e3e
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-07 23:03:55 +0100

    adding uglify to gen one small angular.js file

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
 var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-require('./app_api/models/db'); //models for dbs
-
-
 var http = require('http');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('logger');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressLayouts = require('express-ejs-layouts');
+require('./app_api/models/db');
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
+var app = express();
 
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '/app_server/views'));
+app.set('view engine', 'jade');
 
+var appClientFiles = [
+  'app_client/app.js',
+  'app_client/home/home.controller.js',
+  'app_client/common/services/geolocation.service.js',
+  'app_client/common/services/loc8rData.service.js',
+  'app_client/common/filters/formatDistance.filter.js',
+  'app_client/common/directives/ratingStars/ratingStars.directive.js'
+];
+var uglified = UglifyJS.minify(appClientFiles, { compress : false });
 
-app.set('port',process.env.PORT || 3000);
-app.set('views',path.join(__dirname,'/app_server/views'));
-app.set('view engine','jade');
-// app.set('view engine','ejs')
+fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Script generated and saved:", 'loc8r.min.js');
+  }
+});
 
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
-// app.use(expressLayouts);  
-
-// app.use(express.static(__dirname,'/public')).use(cookieParser());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+// app.use(express.session());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+require('./routes')(app);
+require('./app_api/routes')(app);
 
-require('./routes')(app);  //main
-require('./app_api/routes')(app); //api 
-
-server.listen(app.get('port'),function(){
-  console.log("Server is running at port:" + app.get('port'));
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-module.exports = server;
